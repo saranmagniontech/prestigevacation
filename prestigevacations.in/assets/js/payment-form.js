ae4eaa7b50bd1ad6c14f69e5e6028a5de5633057
saranmagniontech <developers@magnion.co.in>
2026-01-25 21:17:57 +0530

    .

--- prestigevacations.in/assets/js/payment-form.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('input[type="radio"]').on('click', (function () {
-        var inputValue = $(this).attr("value");
-        var targetBox = $("." + inputValue);
-        $(".payment_toggle").not(targetBox).hide();
-        $(targetBox).show();
-    })
-    )
-});
